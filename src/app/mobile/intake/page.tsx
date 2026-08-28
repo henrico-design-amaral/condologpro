83bc06f9882aafdfc25cd1197c3a55c7bec944f6
Henrico Amaral <henrico.creator.amaral@gmail.com>
2026-08-28 14:59:39 -0300

    refactor(scope): remove legacy Next runtime from marketing repo

--- src/app/mobile/intake/page.tsx
+++
@@ -1,9 +0,0 @@
-import { IntakeForm } from "./intake-form";
-
-export default function MobileIntakePage() {
-  return (
-    <main className="min-h-screen bg-neutral-950">
-      <IntakeForm />
-    </main>
-  );
-}
